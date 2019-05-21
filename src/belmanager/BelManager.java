42fc642b98ae0607da1549260961831493bf7013
Simon Melchertsen <warcraft.simon@gmail.com>
2019-05-21 09:42:44 +0200

    asdasd

--- src/belmanager/BelManager.java
+++ src/belmanager/BelManager.java
@@ -16,7 +16,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import javafx.util.converter.LocalDateTimeStringConverter;
 
 /**
  *
--- src/belmanager/GUI/MultiOrderViewController.java
+++ src/belmanager/GUI/MultiOrderViewController.java
@@ -35,7 +35,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;

390af999cc3fd5560419651fb63db8e7439921b5
Simon Melchertsen <warcraft.simon@gmail.com>
2019-05-31 11:52:40 +0200

    Update ConfigFileDao.java

--- src/belmanager/DAL/ConfigFileDao.java
+++ src/belmanager/DAL/ConfigFileDao.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
